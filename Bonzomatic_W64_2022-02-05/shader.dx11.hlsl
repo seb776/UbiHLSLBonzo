3e8867c11fe5a9caa4b9d20415102660fa7e2c1a
seb776 <sebastien.maire@epitech.eu>
2022-02-17 16:50:02 +0100

    Rep function

--- Bonzomatic_W64_2022-02-05/shader.dx11.hlsl
+++ Bonzomatic_W64_2022-02-05/shader.dx11.hlsl
@@ -24,30 +24,38 @@ float2x2 rotation(float a)
   return float2x2(c, -s, s, c);
 }
 
+float sdCircle(float2 uv, float2 pos, float rad)
+{
+  return distance(uv, pos) - rad;
+}
+float sdBox(float2 p, float2 b )
+{
+    float2 d = abs(p)-b;
+    return length(max(d,0.0)) + min(max(d.x,d.y),0.0);
+}
 float4 renderShapes(float2 uv)
 {
-  uv.x += sin(uv.y*25.+fGlobalTime)*0.1;
   float2 ouv = uv;
   float4 col = float4(0.,0.,0.,0.);
   
-  //if (uv.x > -0.1 && uv.x < 0.1)
-  if (abs(uv.x) < 0.1 && abs(uv.y) < 0.1) 
-  {  
-    col = float4(1.,1.,1.,1.)*0.5;
-  }
-  float2 uv2 = uv;
+  float2 rep = float2(0.15,0.15);
+  
+  uv += float2(10.,10.);
+  float2 id = floor((uv+rep*0.5)/rep);
 
-  uv2 += float2(sin(fGlobalTime)*.2,0.1);
-  if (abs(uv2.x) < 0.1 && abs(uv2.y) < 0.1) 
-  {  
-    col += float4(0.,0.,1.,1.);
-  } 
-  float2 uv3 = ouv+float2(-0.1,-0.1);    
-  if (abs(ouv.x) < 0.1 && abs(ouv.y) < 0.1) 
-  {  
-    col += float4(1.,0.,0.,1.);
-  } 
-   
+  uv = fmod(uv+rep*0.5,rep)-rep*0.5;
+  
+  float dist = sdBox(uv, float2(0.05,0.05));
+  
+  
+  dist = min(dist, sdCircle(uv, float2(0.2,0.), 0.2));
+  
+  dist = abs(dist)-0.005;
+  dist = min(dist, abs(sdCircle(uv, float2(0.2,0.3), 0.2))-.05);
+  
+  float4 rgb = lerp(float4(1.,.1,0.1,1.), float4(0.,.1,1.,1.), saturate(sin(id.y*100.+fGlobalTime+id.x*500.)));
+  col = rgb*saturate(-dist*v2Resolution.x/2.0);
+  //*saturate(-dist*v2Resolution.x*0.5);
   return col;
 }
 
@@ -61,7 +69,7 @@ float4 main( float4 position : SV_POSITION, float2 TexCoord : TEXCOORD ) : SV_TA
   // step smoothstep smootherstep
   // round ceil floor
    // sin, cos, tan, asin acos, atan, atan2
-   // ln(a) log(a), exp(a), pow(a, b)
+   // ln(a) log(a), exp(a), pow(a, b), sqrt
   // dot, cross, normalize, reflect, refract 
   // ddx, ddy, fwidth / coarce / fine  
   
