8cbe468f97488c73c3f89f24e834bc52f3bc79f4
seb776 <sebastien.maire@epitech.eu>
2022-03-23 13:43:09 +0100

    playing with 360 texture + different ambient occlusion techniques

--- Bonzomatic_W64_2022-02-05/shader.glsl
+++ Bonzomatic_W64_2022-02-05/shader.glsl
@@ -14,11 +14,24 @@ uniform sampler2D texTex1;
 uniform sampler2D texTex2;
 uniform sampler2D texTex3;
 uniform sampler2D texTex4;
+uniform sampler2D texTex360;
+
 
 in vec2 out_texcoord;
 layout(location = 0) out vec4 out_color; // out_color must be written in order to see anything
 
 #define sat(a) clamp(a, 0., 1.)
+
+float hash11(float seed)
+{
+  return fract(sin(seed*123.456)*123.456);
+}
+float _seed;
+float rand()
+{
+  _seed += 1;
+  return hash11(_seed);
+}
 
 vec3 getCam(vec2 uv, vec3 rd)
 {
@@ -34,26 +47,57 @@ float _cube(vec3 p, vec3 s)
 }
 
 mat2 r2d(float a) { float c = cos(a), s = sin(a); return mat2(c, -s, s, c); }
-
-float map(vec3 p)
+vec2 _min(vec2 a, vec2 b)
 {
+  if (a.x < b.x)
+    return a;
+  return b;
+}
+vec2 map(vec3 p)
+{
+  vec2 acc = vec2(100000.,-1.);
   float shape = length(p)-1.;
   vec3 pc = p+vec3(1.,0,-0.);
   pc.yz *= r2d(fGlobalTime);
   shape = min(shape, _cube(pc, vec3(.5)));
-  return shape;
+  
+  acc = _min(acc, vec2(shape, 0.));
+  
+  acc = _min(acc, vec2(-_cube(p-vec3(0.,-6.5,0.), vec3(7.)), 0.));
+  return acc;
 }
-
+#define PI 3.14159265
 vec3 getNormal(vec3 p)
 {
-  vec2 e = vec2(0.001,0.);
+  vec2 e = vec2(0.01,0.);
   return -normalize(vec3(
-    map(p-e.xyy)-map(p+e.xyy),
-    map(p-e.yxy)-map(p+e.yxy),
-    map(p-e.yyx)-map(p+e.yyx)
+    map(p-e.xyy).x-map(p+e.xyy).x,
+    map(p-e.yxy).x-map(p+e.yxy).x,
+    map(p-e.yyx).x-map(p+e.yyx).x
   ));
   
   return normalize(p);
+}
+
+vec3 getEnv(vec3 rd)
+{
+  vec2 uv = vec2(atan(rd.z, rd.x)/PI, -acos(rd.y)/PI);
+  return pow(texture(texTex360, uv).xyz, vec3(1./2.2));
+}
+
+vec4 trace(vec3 ro, vec3 rd, int steps, float maxdist)
+{
+  vec3 p = ro;
+  for (int i = 0 ; i < 128 && distance(p, ro) < maxdist; ++i)
+  {
+    vec2 res = map(p);
+    if (res.x < 0.01)
+    {
+      return vec4(res.x, distance(p, ro), res.y, float(i));
+    }
+    p += rd*res.x;
+  }
+  return vec4(-1.);
 }
 
 vec3 rdr(vec2 uv)
@@ -62,22 +106,45 @@ vec3 rdr(vec2 uv)
   
   //col = vec3(1.)*(1.-sat((length(uv)-.25)*400.));
   
-  vec3 camPos = vec3(0.,0.,-5.);
+  vec3 ro = vec3(sin(fGlobalTime*.25)*5.,0.,cos(fGlobalTime*.25)*5.);
   vec3 ta = vec3(0,0,0);
-  vec3 rd = normalize(ta-camPos);
+  vec3 rd = normalize(ta-ro);
   rd = getCam(uv, rd);
-  
-  vec3 p = camPos;// + rd;
-  for (int i = 0 ; i < 128; ++i)
+  col = getEnv(rd);
+  vec4 res = trace(ro, rd, 256,20.);
+  if (res.y > 0)
   {
-    float dist = map(p);
-    if (dist < 0.001)
+    vec3 p = ro+rd*res.y;
+    vec3 n= getNormal(p);
+    
+    float ao = 0.;
+    int sampleao = 8;
+    
+   for (int i = 0; i < sampleao; ++i)
     {
-      col = getNormal(p)*.5+.5;
-      break;
-    }
-    p += rd*dist;
+    vec3 sampledir = normalize(vec3(rand(), rand(), rand())-0.5);
+    if (dot(sampledir, n) < 0.0)
+      sampledir = -sampledir;
+    vec4 resao = trace(p+n*0.01, sampledir, 8, 1.5);
+    float fact = 1000.0;
+    if (resao.y > 0.)
+      fact = resao.y;
+    fact = sat(fact/1.5);
+    ao+= fact;
   }
+  ao /= float(sampleao);
+  
+  ao = pow(1.-sat(res.w/128.),.4);
+  float distao = 1.5;
+  ao = sat(map(p+n*0.5*distao).x/distao);
+    col = mix(vec3(1.),(n*.5+.5),.25);
+  col *= sat(ao+0.7);
+
+    //*0.75*getEnv(refl);
+    //vec3 refl = normalize(reflect(rd, n));    
+//    col += 0.5*getEnv(refl);
+  }
+
   
   return col;
 }
@@ -85,7 +152,8 @@ vec3 rdr(vec2 uv)
 void main(void)
 {
 	vec2 uv = (out_texcoord-.5)*(v2Resolution.xy/v2Resolution.xx);
-
+  _seed = texture(texTex2, uv).x+fGlobalTime;
   vec3 col = rdr(uv);
+  
 	out_color = vec4(col,1.);
 }
